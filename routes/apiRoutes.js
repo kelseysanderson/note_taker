d2c75a7f9521fe57213d98f22a081e371a84e82d
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-04-09 16:05:39 -0500

    finished apiRoutes.js to include delete and fixed bug so that when clicked on , notes appear to the right.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,57 @@
 const fs = require('fs');
 const db = require("../db/db.json");
-
+const { nanoid } = require('nanoid');
+const { send } = require('process');
+const { response } = require('express');
 
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
         let data = fs.readFileSync('./db/db.json'); 
         let notes = JSON.parse(data);
-        console.log(notes);
+        // console.log(notes);
         res.json(notes);
     });
 
     app.post('/api/notes', (req, res) => {
-            let newNotes = req.body;
-            db.push(newNotes);
-            let notesJSON = JSON.stringify(db)
-            fs.writeFile('./db/db.json', notesJSON, function (err) {
-                if (err) return console.log(err); 
+        //unique id
+        const uniqId = nanoid(5);
+        req.body.id = uniqId
+
+        // push data to db
+        let newEntry = req.body 
+        let data = fs.readFileSync('./db/db.json'); 
+        db.push(newEntry);
+        // console.log(db)
+        let notesStr= JSON.stringify(db);
+        
+        //write file and display.
+        fs.writeFile('./db/db.json', notesStr, function (err) {
+            if (err) return console.log(err); 
         });
+        response.json(data)
     });
+
+    app.delete('/api/notes/:id', function (req, res) {
+        const clickedNote = req.params.id;
+        console.log(db.length)
+        console.log(clickedNote)
+        for(var i = 0; i < db.length; i++) {
+            console.log()
+            if(db[i].id == clickedNote) {
+                db.splice(i, 1);
+                console.log(db)
+                break;
+            }
+        }
+        let notesStr= JSON.stringify(db);
+
+        fs.writeFile('./db/db.json', notesStr, function (err) {
+            if (err) return console.log(err); 
+        });
+        response.json(data)
+
+    })
 }
 
 
